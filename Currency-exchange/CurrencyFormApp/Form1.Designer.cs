c79ef5edb6118d3a1ed2921bd044254e9380c563
Kirill Ryrmak <rirmakkirill@gmail.com>
2022-07-09 17:27:00 +0300

    Final

--- Currency-exchange/CurrencyFormApp/Form1.Designer.cs
+++ Currency-exchange/CurrencyFormApp/Form1.Designer.cs
@@ -55,9 +55,9 @@ namespace CurrencyFormApp
             this.listViewCurrates.FullRowSelect = true;
             this.listViewCurrates.GridLines = true;
             this.listViewCurrates.HideSelection = false;
-            this.listViewCurrates.Location = new System.Drawing.Point(12, 171);
+            this.listViewCurrates.Location = new System.Drawing.Point(387, 50);
             this.listViewCurrates.Name = "listViewCurrates";
-            this.listViewCurrates.Size = new System.Drawing.Size(752, 286);
+            this.listViewCurrates.Size = new System.Drawing.Size(377, 407);
             this.listViewCurrates.TabIndex = 0;
             this.listViewCurrates.UseCompatibleStateImageBehavior = false;
             this.listViewCurrates.View = System.Windows.Forms.View.Details;
@@ -85,9 +85,9 @@ namespace CurrencyFormApp
             // buttonDownload
             // 
             this.buttonDownload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonDownload.Location = new System.Drawing.Point(689, 8);
+            this.buttonDownload.Location = new System.Drawing.Point(712, 11);
             this.buttonDownload.Name = "buttonDownload";
-            this.buttonDownload.Size = new System.Drawing.Size(75, 23);
+            this.buttonDownload.Size = new System.Drawing.Size(52, 20);
             this.buttonDownload.TabIndex = 1;
             this.buttonDownload.Text = "Load";
             this.buttonDownload.UseVisualStyleBackColor = true;
@@ -95,43 +95,49 @@ namespace CurrencyFormApp
             // 
             // dateTimePickerCurrencyDate
             // 
-            this.dateTimePickerCurrencyDate.Location = new System.Drawing.Point(12, 11);
+            this.dateTimePickerCurrencyDate.Location = new System.Drawing.Point(387, 12);
             this.dateTimePickerCurrencyDate.Name = "dateTimePickerCurrencyDate";
             this.dateTimePickerCurrencyDate.Size = new System.Drawing.Size(145, 20);
             this.dateTimePickerCurrencyDate.TabIndex = 2;
+            this.dateTimePickerCurrencyDate.ValueChanged += new System.EventHandler(this.dateTimePickerCurrencyDate_ValueChanged);
             // 
             // textBoxFilter
             // 
             this.textBoxFilter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxFilter.BackColor = System.Drawing.SystemColors.Window;
-            this.textBoxFilter.Location = new System.Drawing.Point(267, 10);
+            this.textBoxFilter.Location = new System.Drawing.Point(605, 12);
             this.textBoxFilter.Name = "textBoxFilter";
-            this.textBoxFilter.Size = new System.Drawing.Size(416, 20);
+            this.textBoxFilter.Size = new System.Drawing.Size(90, 20);
             this.textBoxFilter.TabIndex = 3;
+            this.textBoxFilter.TextChanged += new System.EventHandler(this.textBoxFilter_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(211, 13);
+            this.label1.Location = new System.Drawing.Point(555, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.Size = new System.Drawing.Size(44, 13);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Filter:";
+            this.label1.Text = "Search:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(99, 90);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(97, 72);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.Size = new System.Drawing.Size(166, 37);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Exchange:";
+            this.label2.Text = "Exchange";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(199, 61);
+            this.comboBox1.Location = new System.Drawing.Point(138, 134);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(86, 21);
             this.comboBox1.TabIndex = 8;
@@ -140,35 +146,39 @@ namespace CurrencyFormApp
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(199, 118);
+            this.comboBox2.Location = new System.Drawing.Point(138, 187);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(83, 21);
+            this.comboBox2.Size = new System.Drawing.Size(86, 21);
             this.comboBox2.TabIndex = 9;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(143, 64);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(82, 137);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(33, 13);
             this.label3.TabIndex = 10;
             this.label3.Text = "From:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(143, 126);
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(92, 190);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(23, 13);
             this.label4.TabIndex = 11;
             this.label4.Text = "To:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(314, 90);
+            this.button1.Location = new System.Drawing.Point(138, 317);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(86, 23);
             this.button1.TabIndex = 12;
             this.button1.Text = "Convert";
             this.button1.UseVisualStyleBackColor = true;
@@ -176,26 +186,28 @@ namespace CurrencyFormApp
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(472, 90);
+            this.textBox1.Location = new System.Drawing.Point(104, 234);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(174, 20);
+            this.textBox1.Size = new System.Drawing.Size(159, 20);
             this.textBox1.TabIndex = 13;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(543, 126);
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(122, 275);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(111, 13);
             this.label5.TabIndex = 14;
             this.label5.Text = "Result of convertation";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(776, 469);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox1);
--- Currency-exchange/CurrencyFormApp/Form1.cs
+++ Currency-exchange/CurrencyFormApp/Form1.cs
@@ -139,6 +139,41 @@ namespace CurrencyFormApp
         {
 
         }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePickerCurrencyDate_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxFilter_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
         
